3e0952cb573cb576412a845e6a00093a491cc9dc
Dan Chilton <dan.chilton@gmail.com>
2020-03-24 17:06:45 -0500

    different test

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,29 +1,14 @@
-<form name="contact" method="POST" data-netlify="true">
+<form name="contact" netlify>
   <p>
     <label>
-      Your Name:
+      Name
       <input type="text" name="name" />
     </label>
   </p>
   <p>
     <label>
-      Your Email:
+      Email
       <input type="email" name="email" />
-    </label>
-  </p>
-  <p>
-    <label>
-      Your Role:
-      <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select>
-    </label>
-  </p>
-  <p>
-    <label>
-      Message:
-      <textarea name="message" />
     </label>
   </p>
   <p>
