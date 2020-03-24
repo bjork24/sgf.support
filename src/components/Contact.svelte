207883efc88a48ec45187293ec3e48c74cf1232a
Dan Chilton <dan.chilton@gmail.com>
2020-03-24 17:52:56 -0500

    pushing another test for deploy preview

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,14 +1,30 @@
-<form name="contact" netlify>
+<script>
+  import serializer from 'form-serialize'
+  function submitForm(e) {
+    console.log(serializer(e.target))
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: serializer(e.target)
+    })
+      .then(data => console.log(data))
+      .catch(error => alert(error))
+  }
+</script>
+
+<form netlify on:submit|preventDefault={submitForm}>
+  <input type="hidden" name="form-name" value="contact" />
+  <input type="hidden" name="action" value="/restaurants" />
   <p>
     <label>
       Name
-      <input type="text" name="name" />
+      <input type="text" name="name" value="dan chilton" />
     </label>
   </p>
   <p>
     <label>
       Email
-      <input type="email" name="email" />
+      <input type="email" name="email" value="dan@dan.com" />
     </label>
   </p>
   <p>
