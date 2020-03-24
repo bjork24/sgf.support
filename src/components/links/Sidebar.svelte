530406b11fe9b073e9907a92d07382af91833f43
Dan Chilton <dan.chilton@gmail.com>
2020-03-24 15:18:52 -0500

    good point for homepage

--- src/components/links/Sidebar.svelte
+++ src/components/links/Sidebar.svelte
@@ -1,5 +1,32 @@
 <script>
   import { link } from 'svelte-routing'
+  import resources from '../resources/data.yml'
+  import ResourceSidebarLink from '../resources/SidebarLink.svelte'
+  import RestaurantSidebarLink from '../restaurants/SidebarLink.svelte'
+
+  const sidebarResources = resources.filter(r => r.sidebar)
 </script>
 
-<a href="/restaurants" use:link>restaurants</a>
+<style>
+  .resources {
+    padding-bottom: 2rem;
+    margin-bottom: 2rem;
+  }
+  .resources a {
+    display: block;
+    text-align: right;
+    color: var(--color5);
+  }
+  .resources :global(.resource-button) {
+    margin-bottom: 1rem;
+  }
+</style>
+
+<div class="resources">
+  {#each sidebarResources as resource}
+    <ResourceSidebarLink {resource} />
+  {/each}
+  <a href="/resources" use:link>More resources...</a>
+</div>
+
+<RestaurantSidebarLink />
